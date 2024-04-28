5881497f1071a3fac2a34d02d7fa0bdddfb498f9
Fauzein Mulya Warman <fauzeinmulyawarman@Fauzeins-MacBook-Pro.local>
2024-04-28 22:58:42 +0700

    menambahkan API mahasiswa

---
+++ tpmodul10_1302223127/Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿namespace tpmodul10_1302223127;
+
+public class Mahasiswa
+{
+    public string nama { set; get; }
+	public string nim { set; get; }
+
+	public Mahasiswa(string nama, string nim)
+	{
+		this.nama = nama;
+		this.nim = nim;
+	}
+
+	
+}
+
+
